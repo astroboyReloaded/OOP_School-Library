c1b6a1cd62eb41b45917de93fc0328d825a67c38
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-07-24 16:18:23 -0600

    Fix can_use_services method to remove repetition

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(name, age = 'Unknown', parent_permission: true)
+  def initialize(name = 'Unknown', age, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,10 +10,10 @@ class Person
   attr_reader :id
 
   def of_age?
-    age > 18
+    age >= 18
   end
 
   def can_use_services?
-    parent_permission == true || age >= 18
+    parent_permission == true || of_age?
   end
 end
